0ca637d3f6bb637b2e45db34bd45525d8234c1a7
unknown <fachrurrozy-pkl@jogjacamp.com>
2025-12-03 11:43:20 +0700

    fitur sosmed

--- resources/views/admin/layouts/main.blade.php
+++ resources/views/admin/layouts/main.blade.php
@@ -6,8 +6,7 @@
     <title>Dashboard Admin - UMKM</title>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-
-    {{-- SweetAlert2 --}}
+    
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
     <style>
